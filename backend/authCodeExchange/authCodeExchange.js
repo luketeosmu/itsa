d6cad552bd5a28dd6044bfd9a6e8879df183bbd1
Wei Khiang <weikhiang92000@gmail.com>
2023-03-06 00:59:49 +0800

    added authCodeExchange server

---
+++ backend/authCodeExchange/authCodeExchange.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const config = require('config');
+
+const app = express();
+const port = 3000; // replace with your desired port number
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  const authCode = req.query.code; // extract auth code from query string
+  const client_id = config.get('client_id'); 
+  const client_secret = config.get('client_secret'); 
+  const bank_end_point = config.get('bank_end_point');
+  const redirect_uri = `http://localhost:${port}`; // replace with your redirect URI
+
+  const formData = {
+    grant_type: 'authorization_code',
+    code: authCode,
+    client_id: client_id, 
+    client_secret: client_secret, 
+    redirect_uri: redirect_uri
+  };
+
+  request.post({ url: `${bank_end_point}/oauth/token`, formData }, (err, response, body) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Access token:', JSON.parse(body).access_token);
+    //   redirect to frontend with the access token
+    res.redirect(`http://localhost:5000?access_token=${JSON.parse(body).access_token}`);
+  });
+
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
