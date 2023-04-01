da56af4c08daa1474b1665f958a73de1190445ff
Wei Khiang <weikhiang92000@gmail.com>
2023-04-01 19:16:14 +0800

    update HostedLoginRedirect

--- backend/hostedLoginRedirect/index.js
+++ backend/hostedLoginRedirect/index.js
@@ -1,12 +1,25 @@
 const querystring = require('querystring');
 const mysql = require('mysql2/promise');
 const bcrypt = require('bcryptjs');
+const jose = require('jose');
+const AWS = require('aws-sdk')
 
 exports.handler = async (event) => {
     /*
     * Generate HTTP redirect response with 302 status code and Location header.
     */
-
+    let secretName = "prod/rds-client-credentials";
+    let config = { region : "ap-southeast-1" }
+    let secretsManager = new AWS.SecretsManager(config);
+    let secretValue = await secretsManager.getSecretValue({SecretId: secretName}).promise();
+    let secretObject = JSON.parse(secretValue.SecretString)
+    const connection = await mysql.createConnection({
+        host: secretObject.host,
+        user: secretObject.username,
+        password: secretObject.password,
+        database: secretObject.dbname
+    });
+    
     const {email, password, auth_code_id} = querystring.parse(event.body);
 
     // get auth_code and redirect_uri using auth_code_id
@@ -24,19 +37,7 @@ exports.handler = async (event) => {
             Location: `${redirect_uri}/error`
         }
     };
-
-    const endpoint = process.env.endpoint;
-    const username = process.env.username;
-    const rdsPassword = process.env.rdsPassword;
-    const databaseName = process.env.databaseName;
-
     
-    const connection = await mysql.createConnection({
-        host: endpoint,
-        user: username,
-        password: rdsPassword,
-        database: databaseName
-    });
     
     const [rows] = await connection.execute("SELECT id, hash_password, salt, role FROM users where email=?", [email]);
     console.log(rows);
@@ -54,35 +55,45 @@ exports.handler = async (event) => {
         console.log("hash_password error");
         return redirectError;
     }
+    
+    // retrieve keys
+    secretName = "prod/keys";
+    config = { region : "ap-southeast-1" }
+    secretsManager = new AWS.SecretsManager(config);
+    secretValue = await secretsManager.getSecretValue({SecretId: secretName}).promise();
+    secretObject = JSON.parse(secretValue.SecretString)
+    console.log("Hello2")
 
     // generate access_token and id_token and refresh_token
-    const access_token = jose.SignJWT({ user: { id, email } })
-        .setProtectedHeader({ alg: 'HS256' })
+    const alg = 'RS256'
+    const privateKey = await jose.importPKCS8(secretObject.g1t4AsymmetricKey, alg)
+    const access_token = await new jose.SignJWT({ user: { id, email } })
+        .setProtectedHeader({ alg: alg })
         .setIssuedAt()
         .setExpirationTime('2h')
         .setIssuer('Bank App')
         .setAudience(redirect_uri)
-        .sign(process.env.g1t4AsymmetricKey);
+        .sign(privateKey);
 
     // generate id_token
-    const id_token = jose.SignJWT({ role })
-        .setProtectedHeader({ alg: 'HS256' })
+    const id_token = await new jose.SignJWT({ role })
+        .setProtectedHeader({ alg: alg })
         .setIssuedAt()
         .setExpirationTime('2h')
         .setIssuer('Bank App')
         .setAudience(redirect_uri)
-        .sign(process.env.g1t4AsymmetricKey);
+        .sign(privateKey);
 
     // generate refresh_token
-    const refresh_token = jose.SignJWT({ refresh: true })
-        .setProtectedHeader({ alg: 'HS256' })
+    const refresh_token = await new jose.SignJWT({ refresh: true })
+        .setProtectedHeader({ alg: alg })
         .setIssuedAt()
         .setExpirationTime('30d')
         .setIssuer('Bank App')
         .setAudience(redirect_uri)
-        .sign(process.env.g1t4AsymmetricKey);
+        .sign(privateKey);
 
-    const secret = jose.base64url.decode(process.env.g1t4SymmetricKey)
+    const secret = jose.base64url.decode(secretObject.g1t4SymmetricKey)
     const encrypted_access_token = await new jose.EncryptJWT({ role, access_token })
         .setProtectedHeader({ alg: 'dir', enc: 'A128CBC-HS256' })
         .setIssuedAt()
@@ -95,7 +106,7 @@ exports.handler = async (event) => {
     const response = {
         statusCode: 301,
         headers: {
-            Location: `${redirect_uri}/authToken/?${stringified_params}`
+            Location: `${redirect_uri}/mybank?${stringified_params}`
         }
     };
 
