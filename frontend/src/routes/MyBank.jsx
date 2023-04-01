8a78cb6297c09035d78e293f7d98ff61772a066a
Wei Khiang <weikhiang92000@gmail.com>
2023-04-01 21:52:00 +0800

    update mybank to handle bank sso flow

--- frontend/src/routes/MyBank.jsx
+++ frontend/src/routes/MyBank.jsx
@@ -7,6 +7,16 @@ import axios from 'axios';
 const MyBank = () => {
     const [searchParams, setSearchParams] = useSearchParams()
     const [role, setRole] = useState('');
+    const [personalInfo, setPersonalInfo] = useState({
+        "sub": "0042e904-0473-48d3-8175-f1fd06db0b64",
+        "email": "nicolas.kihn@dietrich.net",
+        "given_name": "Nicolas",
+        "family_name": "Kihn",
+        "name": "Nicolas Kihn",
+        "birthdate": "1975-04-12T00:00:00.000Z",
+        "gender": "Female",
+        "phone_number": "+967 (103) 878-2610"
+    });
     const [users, setUsers] = useState([
         {
             'email' : "kangchinshen@gmail.com",
@@ -40,109 +50,122 @@ const MyBank = () => {
       }
   // TODO: save the access token to local storage/cookie/memory
     useEffect(() => {
-        console.log("code verifier: " + localStorage.getItem('code_verifier'));
-        console.log("auth_code: " + searchParams.get('code'));
+        if (searchParams.get('bankAccessToken')) {
+            const bankUserInfoUrl = "https://3qhkw6bpzk.execute-api.ap-southeast-1.amazonaws.com/default/bank/oauth/userinfo";
+            const bankAccessToken = searchParams.get('bankAccessToken');
+            const bankIdToken = searchParams.get('bankIdToken');
+            localStorage.setItem("access_token", bankAccessToken);
+            localStorage.setItem("id_token", bankIdToken);
 
-        const postUrl = "https://3qhkw6bpzk.execute-api.ap-southeast-1.amazonaws.com/default/hosted_login_oauth_token";
-
-        const getFromAuthApp = () => {
-            fetch(postUrl)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            })
-        }
-        
-        // USING FETCH METHOD 
-        
-        // const parseJwt = (token) => {
-        //     var base64Url = token.split(".")[0];
-        //     var base64 = decodeURIComponent(atob(base64Url).split('').map((c)=> {
-        //         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        //     }).join(''));
-
-        //     return JSON.parse(base64);
-        // }
-        
-        // console.log(signature);
-        
-
-        const postToAuthApp = () => {
-            fetch(postUrl, {
-                method: 'POST',
-                body: JSON.stringify({
-                    'auth_code' : searchParams.get('code'),
-                    'code_verifier' : localStorage.getItem('code_verifier'),
-                    'client_id' : 'cMZ8riSFzCrLUwDCkd3awhx5pFLURjW5th2aWfm13ws',
-                    'client_secret' : 'PLT2bDFO0zU-8j1pADf-VqzZNMJqaQKyy0K-O5XMGPk'
-                }),
+            fetch(bankUserInfoUrl, {
+                method: 'GET',
                 headers: {
-                    'Content-type': 'application/json',
+                    authorizationToken: bankAccessToken,
                 },
             }).then(response => response.json())
             .then(data => {
-                //access token + refresh token
-                console.log(data)
-                console.log(data["access_token"]);
-                console.log(data["id_token"]);
-                console.log(data["refresh_token"]);
-                localStorage.setItem("access_token", data["access_token"]);
-                localStorage.setItem("id_token", data["id_token"]);
-                localStorage.setItem("refresh_token", data["refresh_token"]);
-                console.log("TEST 1: " + data["id_token"]);
-                // replace with data.token or something idk whats the variable name
-                // let token = JSON.parse(data);
-                // console.log("TEST 2: " + token);
-                let id_token = data["id_token"];
-                // let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
-                // console.log(parseJwt(token["id_token"]));
-                let parts = id_token.split(".");
-                let header = JSON.parse(atob(parts[0]));
-                let payload = JSON.parse(atob(parts[1]));
-                // console.log(parts[2]);
-                // // let signature = atob(parts[2]); //this token signature doesnt work idky
-                console.log(header);
-                console.log(payload.role);
-                //idk whats the role variable name in the token 
-                setRole(payload.role);
-                // setRole("superadmin");
-                if(role === "user") {
-                    //GET own user details and setUsers() -> will be array of len 1 
-                } else {
-                    //GET all user details and setUsers()
-                    //example:
-                    setUsers(
-                        [{
-                            'email' : "kangchinshen@gmail.com",
-                            'firstName' : 'kang',
-                            'lastName' : 'chin shen',
-                            'uid' : "123456789zxc",
-                            'status' : "inactive",
-                            'actions' : "read/write"
-                        },
-                        {
-                            'email' : "chinshenkang@gmail.com",
-                            'firstName' : 'chin',
-                            'lastName' : 'shen kang',
-                            'uid' : "987654321abc",
-                            'status' : "active",
-                            'actions' : "read/write"
-                        },
-                        {
-                            'email' : "shenchinkang@gmail.com",
-                            'firstName' : 'shen',
-                            'lastName' : 'chin kang',
-                            'uid' : "0101010101jkl",
-                            'status' : "active",
-                            'actions' : "read/write"
-                        }])
-                    }
+                console.log(data);
+                setPersonalInfo(data);
             })
-            .catch((err) => {
-                console.log(err.message);
-            });
-        }
-        if(searchParams.get('code') != null) {
+        } else if (searchParams.get('code')) {
+            const postUrl = "https://3qhkw6bpzk.execute-api.ap-southeast-1.amazonaws.com/default/hosted_login_oauth_token";
+            console.log("code verifier: " + localStorage.getItem('code_verifier'));
+            console.log("auth_code: " + searchParams.get('code'));
+            const postToAuthApp = () => {
+                fetch(postUrl, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        'auth_code' : searchParams.get('code'),
+                        'code_verifier' : localStorage.getItem('code_verifier'),
+                        'client_id' : 'cMZ8riSFzCrLUwDCkd3awhx5pFLURjW5th2aWfm13ws',
+                        'client_secret' : 'PLT2bDFO0zU-8j1pADf-VqzZNMJqaQKyy0K-O5XMGPk'
+                    }),
+                    headers: {
+                        'Content-type': 'application/json',
+                    },
+                }).then(response => response.json())
+                .then(data => {
+                    //access token + refresh token
+                    console.log(data)
+                    console.log(data["access_token"]);
+                    console.log(data["id_token"]);
+                    console.log(data["refresh_token"]);
+                    localStorage.setItem("access_token", data["access_token"]);
+                    localStorage.setItem("id_token", data["id_token"]);
+                    localStorage.setItem("refresh_token", data["refresh_token"]);
+                    console.log("TEST 1: " + data["id_token"]);
+                    // replace with data.token or something idk whats the variable name
+                    // let token = JSON.parse(data);
+                    // console.log("TEST 2: " + token);
+                    let id_token = data["id_token"];
+                    // let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+                    // console.log(parseJwt(token["id_token"]));
+                    let parts = id_token.split(".");
+                    let header = JSON.parse(atob(parts[0]));
+                    let payload = JSON.parse(atob(parts[1]));
+                    // console.log(parts[2]);
+                    // // let signature = atob(parts[2]); //this token signature doesnt work idky
+                    console.log(header);
+                    console.log(payload.role);
+                    //idk whats the role variable name in the token 
+                    setRole(payload.role);
+                    // setRole("superadmin");
+                    if(role === "user") {
+                        //GET own user details and setUsers() -> will be array of len 1 
+                    } else {
+                        //GET all user details and setUsers()
+                        //example:
+                        setUsers(
+                            [{
+                                'email' : "kangchinshen@gmail.com",
+                                'firstName' : 'kang',
+                                'lastName' : 'chin shen',
+                                'uid' : "123456789zxc",
+                                'status' : "inactive",
+                                'actions' : "read/write"
+                            },
+                            {
+                                'email' : "chinshenkang@gmail.com",
+                                'firstName' : 'chin',
+                                'lastName' : 'shen kang',
+                                'uid' : "987654321abc",
+                                'status' : "active",
+                                'actions' : "read/write"
+                            },
+                            {
+                                'email' : "shenchinkang@gmail.com",
+                                'firstName' : 'shen',
+                                'lastName' : 'chin kang',
+                                'uid' : "0101010101jkl",
+                                'status' : "active",
+                                'actions' : "read/write"
+                            }])
+                        }
+                })
+                .catch((err) => {
+                    console.log(err.message);
+                });
+            }
+            const getFromAuthApp = () => {
+                fetch(postUrl)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                })
+            }
+            
+            // USING FETCH METHOD 
+            
+            // const parseJwt = (token) => {
+            //     var base64Url = token.split(".")[0];
+            //     var base64 = decodeURIComponent(atob(base64Url).split('').map((c)=> {
+            //         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            //     }).join(''));
+    
+            //     return JSON.parse(base64);
+            // }
+            
+            // console.log(signature);
             postToAuthApp();
         } else {
             let id_token = localStorage.getItem("id_token");
