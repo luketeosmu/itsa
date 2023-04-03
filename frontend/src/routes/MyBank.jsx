fa625d1e876d19486bbf7491073cd0ef48d951ff
Wei Khiang <weikhiang92000@gmail.com>
2023-04-04 04:02:26 +0800

    test refresh token

--- frontend/src/routes/MyBank.jsx
+++ frontend/src/routes/MyBank.jsx
@@ -200,6 +200,7 @@ const MyBank = () => {
                 const postToAuthApp = () => {
                     fetch(postUrl, {
                         method: 'POST',
+                        credentials: 'include',
                         body: JSON.stringify({
                             'auth_code' : searchParams.get('code'),
                             'code_verifier' : localStorage.getItem('code_verifier'),
