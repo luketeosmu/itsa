221c658d4a9072dc56508a66b99d3e5fec27c2b1
luketeosmu <luke.teo.2020@scis.smu.edu.sg>
2023-04-03 21:59:34 +0800

    remove read access

--- frontend/src/routes/BankUsers.jsx
+++ frontend/src/routes/BankUsers.jsx
@@ -94,7 +94,7 @@ const BankUsers = ({ user, setUsers, users, role }) => {
             }
             {role === 'superadmin' ?
                 <td className='flex'>
-                    <label for={'modal-read-user' + user.id}>
+                    {/* <label for={'modal-read-user' + user.id}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="cursor-pointer w-6 h-6 mr-3">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
                         </svg>
@@ -131,7 +131,7 @@ const BankUsers = ({ user, setUsers, users, role }) => {
                             </table>
                             
                         </div>
-                    </div>
+                    </div> */}
                     <label for={'modal-edit-user' + user.id}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="cursor-pointer w-6 h-6 mr-3 stroke-green-600">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
