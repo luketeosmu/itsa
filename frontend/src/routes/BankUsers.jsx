4ce291e3b6ef4b20ca5cd9bad9cbe3443222fb38
luketeosmu <luke.teo.2020@scis.smu.edu.sg>
2023-04-04 17:25:26 +0800

    disable editprofile in navbar for sso

--- frontend/src/routes/BankUsers.jsx
+++ frontend/src/routes/BankUsers.jsx
@@ -100,7 +100,7 @@ const BankUsers = ({ user, setUsers, users, role, loginFlow }) => {
                 :
                 <td className='status-inactive'>{ user.status }</td> 
             }
-            {role === 'superadmin' ?
+            {role === 'superadmin' && loginFlow === "hosted_login" ?
                 <td className='flex'>
                     <label for={'modal-edit-user' + user.id}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="cursor-pointer w-6 h-6 mr-3 stroke-green-600">
@@ -152,15 +152,65 @@ const BankUsers = ({ user, setUsers, users, role, loginFlow }) => {
                         </div>
                     </div>
                 </td> 
-            : 
-                loginFlow === "bank"
-            ?   
-            <Fragment>
-                <td>{user.gender}</td>
-                <td>{user.birthdate} </td>
-                <td>{user.phone_number} </td>
-            </Fragment>
-            :<td></td>
+            :   role === "superadmin" && loginFlow === "bank" && user.id !== localStorage.getItem("currentUser_id") ?
+                <td className='flex'>
+                    <label for={'modal-edit-user' + user.id}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="cursor-pointer w-6 h-6 mr-3 stroke-green-600">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                        </svg>
+                    </label>
+                    <input type="checkbox" id={'modal-edit-user' + user.id} class="modal-toggle" />
+                    <div class="modal">
+                        <div class="modal-box bg-indigo-100 absolute pb-5">
+                            <label for={'modal-edit-user' + user.id} class="btn btn-sm btn-circle absolute right-2 top-2 bg-black-500">✕</label>
+                                <div className="grid grid-cols-2 gap-x-4">
+                                    <div class="form-control mb-3 col-span-2">
+                                        <label class="label">
+                                            <span class="label-text">Email:</span>
+                                        </label>
+                                        <input type="text" value={email} class="input input-bordered w-full" onChange={(e)=> {setEmail(e.target.value)}}/>
+                                    </div>
+                                    <div class="form-control w-full mb-5 col-span-1">
+                                        <label class="label">
+                                            <span class="label-text">Given Name:</span>
+                                        </label>
+                                        <input type="text" value={given_name} class="input input-bordered w-full max-w-xs" onChange={(e) => {setGivenName(e.target.value)}} />
+                                    </div>
+                                    <div class="form-control w-full mb-5 col-span-1">
+                                        <label class="label">
+                                            <span class="label-text">Family Name:</span>
+                                        </label>
+                                        <input type="text" value={family_name} class="input input-bordered w-full max-w-xs" onChange={(e) => {setFamilyName(e.target.value)}} />
+                                    </div>
+                                </div>
+                                <label for={'modal-edit-user' + user.id} className="btn bg-indigo-600 mx-auto flex" onClick={editUser}>Confirm Changes</label>
+                        </div>
+                    </div>
+                    <label for={"modal-delete-user" + user.id} >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="cursor-pointer w-6 h-6 stroke-red-600">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                        </svg>
+                    </label>
+
+                    <input type="checkbox" id={"modal-delete-user" + user.id} class="modal-toggle" />
+                    <div class="modal">
+                        <div class="modal-box w-auto h-auto overflow-x-hidden">
+                            <h3 class="font-bold text-lg">Are you sure you want to delete user {given_name} {family_name}?</h3>
+                            {/* <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p> */}
+                            <div class="modal-action flex justify-center items-center text-center">
+                                <label for={"modal-delete-user" + user.id} className="btn btn-ghost btn-sm bg-zinc-400 mr-5">Cancel</label>
+                                <label for={"modal-delete-user" + user.id} className="btn btn-ghost btn-sm bg-red-600 ml-5" onClick={deleteUser}>Confirm</label>
+                            </div>
+                        </div>
+                    </div>
+                </td> 
+            : loginFlow === "bank" ?
+                <Fragment>
+                    <td>{user.gender}</td>
+                    <td>{user.birthdate} </td>
+                    <td>{user.phone_number} </td>
+                </Fragment>
+            :   <td></td>
             }
         </tr>
     )
