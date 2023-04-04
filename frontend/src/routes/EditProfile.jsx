f7ead43e7fcb769ac04be9e14d7ba97ebf2b6401
luketeosmu <luke.teo.2020@scis.smu.edu.sg>
2023-04-05 05:03:18 +0800

    edit profile input validation

--- frontend/src/routes/EditProfile.jsx
+++ frontend/src/routes/EditProfile.jsx
@@ -6,13 +6,88 @@ const EditProfile = () => {
     const [email, setEmail] = useState(localStorage.getItem("currentUser_email"));
     const [given_name, setGivenName] = useState(localStorage.getItem("currentUser_given_name"));
     const [family_name, setFamilyName] = useState(localStorage.getItem("currentUser_family_name"));
-    
+    const [id, setId] = useState(localStorage.getItem("currentUser_id"));
+    const [originalEmail, setOriginalEmail] = useState(user.email);
+    const [originalGivenName, setOriginalGivenName] = useState(user.given_name);
+    const [originalFamilyName, setOriginalFamilyName] = useState(user.family_name);
+    const [validEmail, setValidEmail] = useState(true);
+    const [validFamilyName, setValidFamilyName] = useState(true);
+    const [validGivenName, setValidGivenName] = useState(true);
+    // useEffect (() => {
+
+    const revertChanges = () => {
+        setEmail(originalEmail);
+        setGivenName(originalGivenName);
+        setFamilyName(originalFamilyName);
+        setValidEmail(true);
+        setValidFamilyName(true);
+        setValidGivenName(true);
+        document.getElementById("emailSpan" + id).style.display = "none";
+        document.getElementById("confirmChange" + id).removeAttribute("disabled");
+    }
+
+    const checkGivenName = (given_name) => {
+        var re = /^[a-zA-Z ]*$/
+        console.log("len:" + given_name.length);
+        console.log("trim: " + given_name.trim().length);
+        if(re.test(given_name) === false || given_name.trim().length === 0) {
+            console.log("invalid given name");
+            setValidGivenName(false);
+            document.getElementById("givenNameSpan" + id).style.display = "block";
+            document.getElementById("confirmChange" + id).setAttribute("disabled", "disabled");
+        } else {
+            setValidGivenName(true);
+            console.log("valid given name");
+            document.getElementById("givenNameSpan" + id).style.display = "none";
+            if(validEmail === true && validFamilyName === true) {
+                document.getElementById("confirmChange" + id).removeAttribute("disabled");
+            }
+        }
+
+    }
+    const checkFamilyName = (family_name) => {
+        var re = /^[a-zA-Z ]*$/
+        if(re.test(family_name) === false || family_name.trim().length === 0) {
+            console.log("invalid family name");
+            setValidFamilyName(false);
+            document.getElementById("familyNameSpan" + id).style.display = "block";
+            document.getElementById("confirmChange" + id).setAttribute("disabled", "disabled");
+        } else {
+            setValidFamilyName(true);
+            console.log("valid family name");
+            document.getElementById("familyNameSpan" + id).style.display = "none";
+            if(validGivenName === true && validEmail == true) {
+                document.getElementById("confirmChange" + id).removeAttribute("disabled");
+            }
+        }
+    }
+    const checkEmail = (email) => {
+        // setEmail(email);
+        var re = /\S+@\S+\.\S+/;
+        console.log("checking email");
+        if(re.test(email) === false || email.trim().length === 0) {
+            console.log("invalid email");
+            setValidEmail(false);
+            document.getElementById("emailSpan" + id).style.display = "block";
+            document.getElementById("confirmChange" + id).setAttribute("disabled", "disabled");
+        } else {
+            setValidEmail(true);
+            console.log("valid email");
+            document.getElementById("emailSpan" + id).style.display = "none";
+            if(validFamilyName === true && validGivenName === true) {
+                document.getElementById("confirmChange" + id).removeAttribute("disabled");
+            }
+        }
+    }
+
     const editUser = () => {
         // console.log(id);
         localStorage.setItem("currentUser_email", email);
         localStorage.setItem("currentUser_given_name", given_name);
         localStorage.setItem("currentUser_last_name", family_name);
-        
+        setOriginalEmail(email);
+        setOriginalGivenName(given_name);
+        setOriginalFamilyName(family_name);
         console.log(email);
         console.log(given_name);
         console.log(family_name);
@@ -67,15 +142,28 @@ const EditProfile = () => {
                     </div>
                     <div>
                         <h1 className='text-2xl font-medium'>Given Name</h1>
-                        <input type="text" value={given_name} onChange={(e)=>{setGivenName(e.target.value)}} className="input input-bordered w-full max-w-md my-2 bg-indigo-100 text-indigo-600 placeholder-indigo-400 text-sm" />
+                        <input type="text" value={given_name} className="input input-bordered w-full max-w-md my-2 bg-indigo-100 text-indigo-600 placeholder-indigo-400 text-sm" 
+                        onChange={(e)=>{
+                            setGivenName(e.target.value);
+                            checkGivenName(e.target.value);
+                        }} />
+                        <span className="text-red-700 font-light text-xs" style={{ display: "none" }} id={"givenNameSpan" + id}>Invalid Name</span>
                     </div>
                     <div className='ml-10'>
                         <h1 className='text-2xl font-medium'>Family Name</h1>
-                        <input type="text" value={family_name} onChange={(e)=> {setFamilyName(e.target.value)}} className="input input-bordered w-full max-w-md my-2 bg-indigo-100 text-indigo-600 placeholder-indigo-400 text-sm" />
+                        <input type="text" value={family_name} className="input input-bordered w-full max-w-md my-2 bg-indigo-100 text-indigo-600 placeholder-indigo-400 text-sm" 
+                        onChange={(e)=> {
+                            setFamilyName(e.target.value);
+                            checkFamilyName(e.target.value)}}/>
+                        <span className="text-red-700 font-light text-xs" style={{ display: "none" }} id={"familyNameSpan" + id}>Invalid Name</span>
                     </div>
                     <div className='col-span-2'>
                         <h1 className='text-2xl font-medium'>Email</h1>
-                        <input type="text" value={email} onChange={(e)=> {setEmail(e.target.value)}} className="input input-bordered w-full my-2 bg-indigo-100 text-indigo-600 placeholder-indigo-400 text-sm" />
+                        <input type="text" value={email} className="input input-bordered w-full my-2 bg-indigo-100 text-indigo-600 placeholder-indigo-400 text-sm" 
+                        onChange={(e)=> {
+                            setEmail(e.target.value);
+                            checkEmail(e.target.value)}}/>
+                        <span className="text-red-700 font-light text-xs" style={{ display: "none" }} id={"emailSpan" + id}>Invalid Email</span>
                     </div>
                     <div className='col-span-2 text-center mt-5'>
                         {/* <button className="btn w-40 bg-indigo-600 mt-6 ml-5 mr-8" >Delete Account</button> */}
@@ -110,7 +198,7 @@ const EditProfile = () => {
                                     </div>
                                 </div>
                             </div> */}
-                        <label for="save-changes-modal" class="btn w-40 bg-indigo-600 mt-6 ml-8 mr-5" onClick={editUser}>Save Changes</label>
+                        <label for="save-changes-modal" class="btn w-40 bg-indigo-600 mt-6 ml-8 mr-5" id={"confirmChange" + id} onClick={editUser}>Save Changes</label>
                         <input type="checkbox" id="save-changes-modal" class="modal-toggle" />
                         <div class="modal">
                             <div class="modal-box">
